ae904f2b0868eeac14606a451f4b170f273033f7
Ravi N <your.email@example.com>
2024-06-08 18:17:26 +0530

    Pool Area Challenge

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("Rectangle width = " + rectangle.getWidth());
+        System.out.println("Rectangle length = " + rectangle.getLength());
+        System.out.println("Rectangle Area = " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(5, 10, 5);
+        System.out.println("Cuboid width = " + cuboid.getWidth());
+        System.out.println("Cuboid length = " + cuboid.getLength());
+        System.out.println("Cuboid Area = " + cuboid.getArea());
+        System.out.println("Cuboid height = " + cuboid.getHeight());
+        System.out.println("Cuboid volume = " + cuboid.getVolume());
+
+    }
+}
